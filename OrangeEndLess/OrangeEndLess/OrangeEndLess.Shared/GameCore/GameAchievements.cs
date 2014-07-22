bafe38a8e76bf02caf4b997575ff087548680163
WenceyWang <henzichao@gmail.com>
2014-07-22 23:42:14 +0800

    u

---
+++ OrangeEndLess/OrangeEndLess/OrangeEndLess.Shared/GameCore/GameAchievements.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System . Collections . Generic;
+using System . Text;
+using System . Threading;
+using System . Threading . Tasks;
+using Windows . Storage;
+using Windows . UI . Xaml;
+using Windows . UI . Xaml . Controls;
+using Windows . UI . Xaml . Input;
+using MVVMSidekick . ViewModels;
+
+namespace OrangeEndLess
+{
+    public partial class Core
+    {
+        public List<Achievement>Achievements=new List<Achievement> ( );
+
+        void AddAchievements ( )
+        {
+            Achievements . Add ( new Achievement ( "" , "" , ( Core cor ) => cor . NumberOfOrangeHaveGet >= 1 ) );
+
+        }
+
+
+    }
+}
